2605d4f7990e0c91b9a1e350f4cf32ea0fe7790a
Disco <jjotasaimon@gmail.com>
2023-07-12 23:47:29 +0700

    Layout and navigation ready

---
+++ components/Layout.tsx
@@ -0,0 +1,35 @@
+// import fonts
+import { Sora } from '@next/font/google'
+
+// font settings
+const sora = Sora({
+  subsets: ['latin'],
+  variable: '--font-sora',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800']
+})
+
+// import components
+import Nav from './Nav'
+import Header from './Header'
+import TopLeftImg from './TopLeftImg'
+import React from 'react'
+
+// props
+
+type LayoutProps = {
+  children: React.ReactNode
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return <div className={`
+  page bg-site text-white bg-cover bg-no-repeat ${sora.variable}
+  font-sora relative`
+  }>
+    <TopLeftImg />
+    <Nav />
+    <Header />
+    {children}
+  </div>;
+};
+
+export default Layout;
