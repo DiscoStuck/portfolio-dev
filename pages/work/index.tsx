c637e6c67d95f093a23a36bf44d8b6f1e17d58c4
Disco <jjotasaimon@gmail.com>
2023-07-30 11:08:59 +0700

    Sections removed. Minor adjustments. Pre-migration to app

--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -12,20 +12,9 @@ const Work = () => {
     <div className='h-full bg-primary/30 py-36 flex items-center'>
       <Circles />
       <div className='container mx-auto'>
-        <div className='flex flex-col xl:flex-row gap-x-8'>
-          {/* text*/}
-          <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4
-          xl:mb-0'>
-            <motion.h2 variants={fadeIn('up', 0.3)} initial='hidden' animate='show'
-              exit='hidden' className='h2 xl:mt-12'>
-              My work <span className='text-accent'>.</span>
-            </motion.h2>
-            <motion.p variants={fadeIn('up', 0.4)} initial='hidden' animate='show'
-              exit='hidden' className='mb-4 max-w-[400px] mx-auto lg:mx-0'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A eveniet
-              dignissimos distinctio tempore harum laudantium. Beatae prespiciatis
-              mollitia animi quaerat?
-            </motion.p>
-          </div>
+        <div className='flex flex-col gap-x-8 items-center justify-center mt-16'>
+          {/* header */}
+          <h1 className='h1'><span className='text-accent'>My </span>Work</h1>
           <motion.div variants={fadeIn('down', 0.6)} initial='hidden' animate='show'
             exit='hidden' className='w-full xl:max-w-[65%]'>
             {/* slider */}
